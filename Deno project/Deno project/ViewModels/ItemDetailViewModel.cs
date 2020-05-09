bebcca9f9e03e251c1d84b9492ad27f2ae4044e7
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2020-05-09 21:48:26 +0530

    Add project files.

---
+++ Deno project/Deno project/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Deno_project.Models;
+
+namespace Deno_project.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
